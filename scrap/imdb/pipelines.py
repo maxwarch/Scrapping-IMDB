1e43b03a2abc7474a9e442a96de901b37fcd9b93
Maxime KUIL <maximekuil@gmail.com>
2024-03-18 11:10:47 +0100

    refactor OK

--- scrap/imdb/pipelines.py
+++ scrap/imdb/pipelines.py
@@ -5,25 +5,28 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
 from db.database import connect_db
-from imdb.items import ImdbItem
+from itemadapter import ItemAdapter
 from utils.environment import get_env
 
+from .items import ImdbItem
 
-class ImdbPipeline: 
-    def open_spider(self, spider):
+
+class ImdbPipeline:
+    def __init__(self) -> None:
         self.client = connect_db()
-        #print('********* open_spider')
-        db = self.client[get_env('FILM_DB')]
-        self.films = db['films']
+
+    def open_spider(self, spider):
+        # self.client = connect_db()
+        # print('********* open_spider')
+        db = self.client[get_env("FILM_DB")]
+        self.films = db["films"]
 
     def close_spider(self, spider):
-        #print('********* close_spider')
+        # print('********* close_spider')
         self.client.close()
 
     def process_item(self, item: ImdbItem, spider):
-        item['duration'] = item['duration'].total_seconds()
+        item["duration"] = item["duration"].total_seconds()
         self.films.insert_one(ItemAdapter(item).asdict())
         return item
